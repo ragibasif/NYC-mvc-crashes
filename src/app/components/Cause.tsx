a6d8a4b339536a31c6b885c7bd1ec87dd9e81b4a
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-02-05 19:28:23 -0500

    update: footer

---
+++ src/app/components/Cause.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+    <h1>NYC Motor Vehicle Collisions Exploratory Data Analysis and Visualization</h1>
+    <p>Utilized traffic collision data from NYC Open Data to analyze and visualize traffic collisions in New York City between 07/01/2012 to 01/20/2025. The total number of crashes analyzed is 2,150,966.</p>
+    </div>
+  );
+}
